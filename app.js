58dbec430c88045dda05169376c4fa217c6e098a
ac-asks <aprilchi4@gmail.com>
2018-12-11 16:37:58 +1100

    added event listener

--- app.js
+++ app.js
@@ -5,17 +5,7 @@
 // BUDGET CONTROLLER MODULE
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    // Some code
 
 })();
 
@@ -23,21 +13,41 @@ var budgetController = (function() {
 // UI MODULE
 var UIController = (function() {
 
-
+    // Some code
 
 
 })();
 
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    // pass other 2 modules as arguments to this one so we can connect them
-    var z = budgetCtrl.publicTest(5);
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-            // This is the only way we can access z from the outside
-        }
+
+    var ctrlAddItem = function() {
+
+              // 1. Get the field input data
+
+        // 2. Add the item to te budget controller
+
+        // 3. Add the item to te UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+
+        console.log('It works');
     }
 
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        
+        // whcih is for older browsers that don't have this keycode
+        // property
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem(); // call the add item function
+        }
+
+    });
 })(budgetController, UIController);
