eab3055d384a1b460bf436202a882b2e2e6b309b
ac-asks <aprilchi4@gmail.com>
2019-01-02 19:04:51 +1100

    ui displays correct budget

--- app.js
+++ app.js
@@ -115,7 +115,11 @@ var UIController = (function() {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
 
@@ -169,6 +173,23 @@ var UIController = (function() {
                 // clear the fields
                 current.value = "";
             });
+
+            fieldsArr[0].focus();
+        },
+
+        // displays budget onto the UI
+        displayBudget: function(obj) {
+            
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+        
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+
         },
 
         getDOMstrings: function() { // exposing DOMStrings to public
@@ -213,7 +234,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         //3. Display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     };
 
 
@@ -246,7 +267,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     return { // event listeners only going to be set up as soon as we call the init function
         init: function() {
             // Starts the program
-            console.log('Application has started')
+            console.log('Application has started');
+            UICtrl.displayBudget(
+                {
+                    budget: 0,
+                    totalInc: 0,
+                    totalExp: 0,
+                    percentage: -1
+                }
+            );
             setupEventListeners();
         }
     }
